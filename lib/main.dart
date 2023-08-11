ff5f140c2460d0197e2fb03ed15140e4a1a73cb8
J-P Nurmi <jp.nurmi@canonical.com>
2023-08-11 13:38:27 +0200

    refactor: switch to ubuntu-desktop-provision

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:ubuntu_desktop_installer/installer.dart';
+import 'package:ubuntu_bootstrap/ubuntu_bootstrap.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:ubuntu_flavor/ubuntu_flavor.dart';
 import 'package:yaru/yaru.dart';
 
 import 'slides.dart';
@@ -7,16 +8,13 @@ import 'slides.dart';
 Future<void> main(List<String> args) {
   return runInstallerApp(
     args,
-    flavor: FlavorData(
+    flavor: const UbuntuFlavor(
+      id: 'ubuntu-flavor',
       name: 'Ubuntu FLAVOR',
-      theme: yaruMagentaLight,
-      darkTheme: yaruMagentaDark,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
     ),
-    slides: [
-      firstSlide,
-      secondSlide,
-      thirdSlide,
-    ],
+    theme: yaruMagentaLight,
+    darkTheme: yaruMagentaDark,
+    localizationsDelegates: AppLocalizations.localizationsDelegates,
+    slides: installationSlides,
   );
 }
